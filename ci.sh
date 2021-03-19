9448489ceb3c7ef09cbb41571d98e1e29781de64
Little-W <1405481963@qq.com>
2021-03-19 14:23:16 +0800

    add clang compiler

--- ci.sh
+++ ci.sh
@@ -37,7 +37,6 @@ function do_deps() {
         git \
         libelf-dev \
         libssl-dev \
-        lld \
         make \
         llvm \
         ninja-build \
@@ -48,10 +47,6 @@ function do_deps() {
 
     git config --global user.email "1405481963@qq.com"
     git config --global user.name "Little-W"
-    git clone --depth 1 https://github.com/kdrag0n/proton-clang ~/tc
-    sudo dd if=/dev/zero of=/swapfile bs=1M count=2048
-    sudo mkswap /swapfile
-    sudo swapon /swapfile
 }
 
 function do_upload() {
@@ -59,13 +54,22 @@ function do_upload() {
     # Generate build info
 	rel_date="$(date "+%Y%m%d")" # ISO 8601 format
 	clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
-    git clone --depth 1 git@github.com:Little-W/Sakura-ClangBuiltLinux.git ~/cl/
-    mv ~/cl/.git install/.git
-    mv ~/cl/README.md install/README.md
-    cd  install
-    git add -A -f
-    git commit -am "Update to $rel_date build (Clang Version: $clang_version)"
-    git push
+	if [ -d "install" ]; then
+        git clone --depth 1 git@github.com:Little-W/Sakura-ClangBuiltLinux.git ~/cl/
+        mv ~/cl/.git install/.git
+        mv ~/cl/README.md install/README.md
+        cd  install
+  	git add -A -f
+        git commit -am "Update to $rel_date build (Clang Version: $clang_version)"
+        git push
+        
+        else 
+         git add build/llvm/stage1
+	 git add build/llvm/stage2
+   	 git add build/llvm/stage3    
+   	 git commit -m "upload build cache $rel_date"
+        fi
+        
 }
 
 parse_parameters "${@}"
