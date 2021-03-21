40d59bdd0b9c2d0d9fd6c6b3872adc32f3991a5b
Little-W <1405481963@qq.com>
2021-03-21 03:06:53 +0000

    upload build cache 20210321

---
+++ build/llvm/stage1/include/llvm/Frontend/OpenACC/ACC.inc.d
@@ -0,0 +1,1 @@
+include/llvm/Frontend/OpenACC/ACC.inc: /home/runner/work/tc-build/tc-build/llvm-project/llvm/include/llvm/Frontend/Directive/DirectiveBase.td
--- build/llvm/stage1/include/llvm/Support/VCSRevision.h
+++ build/llvm/stage1/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "82855b9bfd1177a2bc2c677beebd2ab273869fe2"
+#define LLVM_REVISION "1168a3ad990d503de7eaa9ef193d52314ba73744"
 #define LLVM_REPOSITORY "https://github.com/Little-W/tc-build"
