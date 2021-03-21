5c9c61c870c08b02518e44d8dfc74300bef59a5d
Little-W <1405481963@qq.com>
2021-03-21 08:33:52 +0000

    upload build cache 20210321

--- build/llvm/stage1/include/llvm/Support/VCSRevision.h
+++ build/llvm/stage1/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "5aa6cdc69220cd459fe0b89a496766bff106603f"
+#define LLVM_REVISION "a471221326bef4286ec313bcf3b6ca7d63867b32"
 #define LLVM_REPOSITORY "https://github.com/Little-W/tc-build"
